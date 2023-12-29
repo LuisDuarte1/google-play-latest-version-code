ee0a9fb14464d4e919e1988c89ccde0568504781
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-12-29 17:46:15 +0000

    Modify CI and remove tests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as core from '@actions/core'
-import { create } from 'domain'
 import { writeFile } from 'fs/promises'
 import { google } from 'googleapis'
-
 
 /**
  * The main function for the action.
@@ -10,34 +8,38 @@ import { google } from 'googleapis'
  */
 export async function run(): Promise<void> {
   try {
-    core.debug("Reading service account json file")
-    const service_account_content: string = core.getInput('google_service_account_json')
+    core.debug('Reading service account json file')
+    const service_account_content: string = core.getInput(
+      'google_service_account_json'
+    )
     await writeFile('/tmp/google-service-account.json', service_account_content)
 
     const auth = new google.auth.GoogleAuth({
       keyFile: '/tmp/google-service-account.json',
       scopes: ['https://www.googleapis.com/auth/androidpublisher']
     })
-    
+
     google.options({
-      auth: auth
+      auth
     })
 
     //first we create a edit in order to access the package bundle list
     const androidDevelopersAPI = google.androidpublisher({
-      version: "v3"
+      version: 'v3'
     })
 
-    const packageName:string = core.getInput('package_name')
+    const packageName: string = core.getInput('package_name')
 
     const createEditReq = await androidDevelopersAPI.edits.insert({
       packageName
     })
 
     const editId = createEditReq.data.id
-    
-    if(editId === null){
-      throw Error(`The API did not return a editId. Response Status: ${createEditReq.status}`)
+
+    if (editId === null) {
+      throw Error(
+        `The API did not return a editId. Response Status: ${createEditReq.status}`
+      )
     }
 
     const bundlesReq = await androidDevelopersAPI.edits.bundles.list({
@@ -47,15 +49,14 @@ export async function run(): Promise<void> {
 
     const bundles = bundlesReq.data.bundles
 
-    if(bundles === undefined || bundles.length === 0){
-      throw Error(`The API did not return the list of bundles. Response Status: ${createEditReq.status}. It's maybe possible that you still don't have a bundle submitted to the Google Play Console.`)
+    if (bundles === undefined || bundles.length === 0) {
+      throw Error(
+        `The API did not return the list of bundles. Response Status: ${createEditReq.status}. It's maybe possible that you still don't have a bundle submitted to the Google Play Console.`
+      )
     }
 
     const lastBundle = bundles.at(-1)
     core.setOutput('latest_version_code', lastBundle?.versionCode)
-    
-
-
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
